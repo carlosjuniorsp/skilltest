e1639d02c90910bd2bead1e9669b0bcb7899003c
Carlos Junior <carlosdossantosjuniorsp@gmail.com>
2022-01-29 17:15:39 -0300

    listing user mock

--- skilltest/src/pages/Home/index.js
+++ skilltest/src/pages/Home/index.js
@@ -1,6 +1,37 @@
 import React from "react";
 import Header from "../../components/Header";
 
+const people = [
+  {
+    name: 'Carlos Junior',
+    age: '29',
+    cpf: '42976592047',
+    city: 'Suzano',
+    state: 'SP',
+  },
+  {
+    name: 'Carlos Junior',
+    age: '29',
+    cpf: '42976592047',
+    city: 'Suzano',
+    state: 'SP',
+  },
+  {
+    name: 'Carlos Junior',
+    age: '29',
+    cpf: '42976592047',
+    city: 'Suzano',
+    state: 'SP',
+  },
+  {
+    name: 'Carlos Junior',
+    age: '29',
+    cpf: '42976592047',
+    city: 'Suzano',
+    state: 'SP',
+  },
+]
+
 const Home = () => {
   return (
     <>
@@ -8,8 +39,81 @@ const Home = () => {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-              1 - Usuario <br/>
-              2 - Usuario
+            <div className="flex flex-col">
+              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="bg-indigo-700">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                          >
+                            Nome
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                          >
+                            Idade
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                          >
+                            CPF
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                          >
+                            Cidade
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-200 uppercase tracking-wider"
+                          >
+                            Estado
+                          </th>
+                          <th scope="col" className="relative px-6 py-3">
+                            <span className="sr-only">Edit</span>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="bg-white divide-y divide-gray-200">
+                        {people.map((person) => (
+                          <tr key={person.email}>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="flex items-center">
+                                <div className="text-sm font-medium text-gray-900">{person.name}</div>                              
+                              </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">{person.age}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">{person.cpf}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">{person.city}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm text-gray-900">{person.state}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                              <a href="/" className="text-indigo-600 hover:text-indigo-900">
+                                Edit
+                              </a>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
